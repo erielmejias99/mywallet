d56982dc96a5c03289820185fcf81fe671133518
Eriel <erielmejias99@gmail.com>
2021-06-27 19:57:03 -0400

    Create method of transaction created

--- transaction/repository.go
+++ transaction/repository.go
@@ -1,6 +1,12 @@
 package transaction
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/go-playground/validator"
+	"github.com/mitchellh/mapstructure"
+	"github.com/wailsapp/wails/runtime"
+	"gorm.io/gorm"
+)
 
 type TransactionI interface {
 	Create( map[string]interface{} ) (Transaction, error)
@@ -19,5 +25,29 @@ func GetTransactionRep( db * gorm.DB) *TransactionRep{
 }
 
 func (receiver TransactionRep) Create(transaction map[string]interface{}) (Transaction, error) {
-	return Transaction{}, nil
+	var newTransaction Transaction
+
+	// Decode javaScript object
+	err := mapstructure.Decode( transaction, &newTransaction )
+	if err != nil{
+		return newTransaction, err
+	}
+
+	// Validate fields
+	val := validator.New()
+	err = val.Struct( &newTransaction )
+	if err != nil{
+		return newTransaction, err
+	}
+
+	// Save Model in DataBase
+	result := receiver.db.Create( &newTransaction )
+	if result.Error != nil {
+		logger := runtime.NewLog()
+		logger.New(fmt.Sprintf( "Error getting currency from DB: %s", result.Error.Error() ) )
+		return newTransaction, result.Error
+	}
+
+	return newTransaction, nil
+
 }
