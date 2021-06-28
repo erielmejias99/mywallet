83342093dec9c85bae2a70de06fdb6807192ee3e
Eriel <erielmejias99@gmail.com>
2021-06-28 00:21:01 -0400

    Refactoring transactions

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,10 +1,7 @@
 package transaction
 
 import (
-	"errors"
 	"gorm.io/gorm"
-	"math"
-	currency "wallet/currency"
 )
 
 type Transaction struct{
@@ -13,38 +10,4 @@ type Transaction struct{
 	Amount int `gorm:"" json:"amount" mapstructure:"amount" validate:"required,min=0"`
 	Description string `gorm:"" json:"description" mapstructure:"description" validate:"max=255"`
 	Reason Reason
-}
-
-func ( t *Transaction ) GetCurrency(tx*gorm.DB) (*currency.Currency, error )  {
-	var currencyWallet currency.Currency
-	resp := tx.Find( &currencyWallet, t.CurrencyID )
-	if resp.Error != nil{
-		return &currencyWallet, resp.Error
-	}
-	return &currencyWallet, nil
-}
-
-func (t *Transaction) AfterSave(tx*gorm.DB) ( error )  {
-
-	currencyWallet, err := t.GetCurrency(tx)
-	if err != nil{
-		return errors.New("error getting Currency of the transaction")
-	}
-
-	// Check if the amount of the transaction is negative and there is such amount.
-	if t.Amount < 0{
-		if currencyWallet.Balance >= int(math.Abs( float64( t.Amount ) )) {
-			return errors.New("not enough balance available")
-		}
-	}
-
-	// Update the currency balance
-	currencyWallet.Balance += t.Amount
-
-	resp := tx.Update( "balance", currencyWallet )
-	if resp.Error != nil {
-		return resp.Error
-	}
-
-	return nil
 }
--- transaction/transaction_repository.go
+++ transaction/transaction_repository.go
@@ -1,11 +1,14 @@
 package transaction
 
 import (
+	"errors"
 	"fmt"
 	"github.com/go-playground/validator"
 	"github.com/mitchellh/mapstructure"
 	"github.com/wailsapp/wails/runtime"
 	"gorm.io/gorm"
+	"math"
+	"wallet/currency"
 )
 
 type TransactionI interface {
@@ -46,6 +49,33 @@ func (receiver TransactionRep) Create(transaction map[string]interface{}) (Trans
 		logger := runtime.NewLog()
 		logger.New(fmt.Sprintf( "Error getting currency from DB: %s", result.Error.Error() ) )
 		return newTransaction, result.Error
+	}
+
+	var currencyWallet currency.Currency
+	resp := receiver.db.Find( &currencyWallet, newTransaction.CurrencyID )
+	if resp.Error != nil{
+		return newTransaction, resp.Error
+	}
+
+	// Check if the amount of the transaction is negative and there is such amount.
+	if newTransaction.Amount < 0{
+		if currencyWallet.Balance >= int(math.Abs( float64( newTransaction.Amount ) )) {
+			return newTransaction, errors.New("not enough balance available")
+		}
+	}
+
+	// Update the currency balance
+	currencyWallet.Balance += newTransaction.Amount
+
+	//validate after update field
+	err = val.Struct(&currencyWallet)
+	if err != nil {
+		return newTransaction,err
+	}
+
+	resp = reasonRepository.db.Update( "balance", currencyWallet )
+	if resp.Error != nil {
+		return newTransaction,resp.Error
 	}
 
 	return newTransaction, nil
