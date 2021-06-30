a47e50ca17fd59191279073cc852a766202090a1
Eriel <erielmejias99@gmail.com>
2021-06-29 21:31:28 -0400

    Refactor currency model

--- models/currency.go
+++ models/currency.go
@@ -2,13 +2,12 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"wallet/models/transaction"
 )
 
 type Currency struct{
 	Name string `gorm:"unique" json:"name" mapstructure:"name" validate:"required,min=3,max=5"`
 	Balance int `gorm:"not null" json:"balance" mapstructure:"balance" validate:"eq=0"`
 	USDChange float32 `gorm:"not null" json:"usd_change" mapstructure:"usd_change" validate:"min=0"`
-	Transactions []transaction.Transaction
+	Transactions []Transaction
 	gorm.Model
 }
