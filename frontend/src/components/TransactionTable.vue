4baa05dd69e4c6515a3bdd6c2ad7766784896c55
Eriel <erielmejias99@gmail.com>
2021-06-30 03:06:00 -0400

    Create tranasactionTable component

--- frontend/src/components/TransactionTable.vue
+++ frontend/src/components/TransactionTable.vue
@@ -1,10 +1,80 @@
 <template>
-$END$
+  <v-data-table
+  :items="transactions"
+  :headers="headers"
+  >
+    <template #no-data>
+      <v-alert v-if="err != null" type="error">
+        {{ err }}
+      </v-alert>
+      <div v-else>
+        No data
+      </div>
+    </template>
+
+  </v-data-table>
 </template>
 
 <script>
 export default {
-name: "TransactionTable"
+  name: "TransactionTable",
+  data: ()=>({
+    transactions: [],
+    err: null,
+    headers:[
+      {
+        text: 'Id',
+        value: 'ID'
+      },
+      {
+        text: "Amount",
+        value: "amount"
+      },
+      {
+        text: "Reason",
+        value: "reason"
+      },
+      {
+        text: "Description",
+        value: "description"
+      },
+      {
+        text: "Date",
+        value: "CreatedAt"
+      }
+    ]
+  }),
+  props:{
+    currency_id:{
+      type: Number,
+      required: false,
+      default: 0
+    }
+  },
+  methods:{
+    loadTransactions: function loadTransactions(){
+      this.loading = true;
+      this.err = null;
+      window.backend.TransactionController.List( this.currency_id )
+        .then( resp=>{
+          this.transactions = resp;
+          console.log( resp );
+          this.loading = false;
+        })
+        .catch( err=>{
+          this.err = err;
+          this.transactions = [];
+        })
+    }
+  },
+  watch:{
+    currency_id: function(){
+      this.loadTransactions()
+    }
+  },
+  mounted() {
+    this.loadTransactions();
+  }
 }
 </script>
 
