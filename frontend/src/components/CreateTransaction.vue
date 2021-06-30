90c92600a44a0847215b76f4f3b2d25f087d2e77
Eriel <erielmejias99@gmail.com>
2021-06-30 03:06:41 -0400

    Add transaction table to the dashboard, Start CreateTransaction Modal

--- frontend/src/components/CreateTransaction.vue
+++ frontend/src/components/CreateTransaction.vue
@@ -1,10 +1,27 @@
 <template>
-$END$
+  <v-dialog v-model="dialog">
+    <template #activator="{attrs,on}">
+      <span v-on="on" v-bind:class="attrs">
+        <slot></slot>
+      </span>
+    </template>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-name: "CreateTransaction"
+  name: "CreateTransaction",
+  data:()=>({
+    transaction:{
+      amount: 0,
+    }
+  }),
+  props:{
+    negative: {
+      type: Boolean,
+      required: true
+    }
+  }
 }
 </script>
 
