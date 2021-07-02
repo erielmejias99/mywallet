26fb5fe35a01e1db3cb58b8f5a3bff70c1176e2d
Eriel <erielmejias99@gmail.com>
2021-07-01 23:25:51 -0400

    Remove Navigation Drawer, Not needed for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,5 @@
 <template>
   <v-app>
-    <v-navigation-drawer app permanent expand-on-hover clipped>
-      <v-list>
-
-        <v-list-item>
-          <v-list-item-avatar>
-            <v-avatar color="red">
-              <v-img :src="require('./assets/wallpaper2.jpg')"></v-img>
-            </v-avatar>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>Eriel Mejias</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item v-for="item in navItems" :key="item.label" link :to="item.to">
-          <v-list-item-icon>
-            <v-icon color="orange">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{item.label}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
     <v-main>
       <router-view></router-view>
