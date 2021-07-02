f3a82f0b58bb3a57ccdc4a3415316669c917730a
Eriel <erielmejias99@gmail.com>
2021-07-01 23:28:39 -0400

    Fix some untested parts

--- models/currency.go
+++ models/currency.go
@@ -8,6 +8,6 @@ type Currency struct{
 	Name string `gorm:"unique" json:"name" mapstructure:"name" validate:"required,min=3,max=5"`
 	Balance int `gorm:"not null" json:"balance" mapstructure:"balance" validate:"eq=0"`
 	USDChange float32 `gorm:"not null" json:"usd_change" mapstructure:"usd_change" validate:"min=0"`
-	Transactions []Transaction
+	Transactions []Transaction `json:"transactions"`
 	gorm.Model
 }
--- models/transaction.go
+++ models/transaction.go
@@ -4,10 +4,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Transaction struct{
+type Transaction struct {
 	gorm.Model
-	CurrencyID  uint `json:"currency_id" mapstructure:"currency_id" validate:"-"`
-	Amount      int `gorm:"" json:"amount" mapstructure:"amount" validate:"required,min=0"`
+	CurrencyID  uint   `json:"currency_id" mapstructure:"currency_id" validate:"-"`
+	Amount      int    `gorm:"" json:"amount" mapstructure:"amount" validate:"required,min=0"`
 	Description string `gorm:"" json:"description" mapstructure:"description" validate:"max=255"`
-	Reason string `gorm:"" json:"reason" mapstructure:"reason" validate:"required,min=3,max=255"`
+	Reason      string `gorm:"" json:"reason" mapstructure:"reason" validate:"required,min=3,max=50"`
 }
