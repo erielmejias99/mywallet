7414120c60fe8b52ae0242520e923216b3090def
Eriel <erielmejias99@gmail.com>
2021-07-04 03:56:54 -0400

    Change DB allocator path

--- database/connection.go
+++ database/connection.go
@@ -9,7 +9,7 @@ var db * gorm.DB;
 func GetDB() * gorm.DB{
 	if db == nil{
 		var err error
-		db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+		db, err = gorm.Open(sqlite.Open("database/database.sqlite3"), &gorm.Config{})
 		if err != nil{
 			panic( "Error connecting to DB.")
 		}
