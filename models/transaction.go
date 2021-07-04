37c59cbc95ee31dfe553b550441aca3032b20e83
Eriel <erielmejias99@gmail.com>
2021-07-04 03:44:38 -0400

    remove min=0 validation, extraction transaction will be negative

--- models/transaction.go
+++ models/transaction.go
@@ -7,7 +7,7 @@ import (
 type Transaction struct {
 	gorm.Model
 	CurrencyID  uint   `json:"currency_id" mapstructure:"currency_id" validate:"-"`
-	Amount      int    `gorm:"" json:"amount" mapstructure:"amount" validate:"required,min=0"`
+	Amount      int    `gorm:"" json:"amount" mapstructure:"amount" validate:"required"`
 	Description string `gorm:"" json:"description" mapstructure:"description" validate:"max=255"`
 	Reason      string `gorm:"" json:"reason" mapstructure:"reason" validate:"required,min=3,max=50"`
 }
