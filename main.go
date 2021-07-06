5b028499a9753db00a0b09ebea1475eddbd6f014
Eriel <erielmejias99@gmail.com>
2021-07-06 01:17:48 -0400

    before desaster

--- main.go
+++ main.go
@@ -8,38 +8,44 @@ import (
     "wallet/database"
 )
 
+//go:embed frontend/dist/app.js
+var js string
+
+//go:embed frontend/dist/app.css
+var css string
 
 func main() {
 
     db := database.GetDB()
 
-    if os.Getenv("MIGRATE" ) == "true"{
-        err := InitMigrations( db )
-        if err != nil{
-            panic("Error creating migrations" )
-        }
+    //if os.Getenv("MIGRATE" ) == "true"{
+    err := InitMigrations( db )
+    if err != nil{
+        panic("Error creating migrations" )
     }
+    //}
 
     if os.Getenv("RUNAPP") == "false"{
         return
     }
 
+    currencyController := controllers.CurrencyController{Db: db}
+    transactionController := controllers.TransactionController{Db: db}
+
     app := wails.CreateApp(&wails.AppConfig{
         Width:  1024,
         Height: 500,
+        JS: js,
+        CSS: css,
         Title:  "wallet",
         Colour: "#131313",
     })
 
-    currencyController := controllers.CurrencyController{Db: db}
-    transactionController := controllers.TransactionController{Db: db}
-    reasonController := controllers.ReasonController{Db: db}
 
     app.Bind(&currencyController)
     app.Bind(&transactionController)
-    app.Bind(&reasonController)
 
-    err := app.Run()
+    err = app.Run()
     if err != nil{
         panic("Error running application" )
     }
